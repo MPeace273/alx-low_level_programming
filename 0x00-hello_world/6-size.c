e55cc9d7777716d6efa70d831b150cb6235f5f7f
root <buorosolomon273@gmail.com>
2022-09-08 19:25:38 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function                                                                                                        
+
+*                                                                                                                                                    
+
+* Description: using the main function                                                                                                               
+
+* this program prints "Programing is like building a multilingual puzzle                                                                             
+
+* Return: 0                                                                                                                                          
+
+*/                                                                                                                                                   
+
+int main (void)                                                                                                                                      
+
+{                                                                                                                                                    
+
+        char c;                                                                                                                                      
+
+        int i,                                                                                                                                       
+
+        long ll;                                                                                                                                     
+
+        long long lll;                                                                                                                               
+
+        fload f;                                                                                                                                     
+
+                                                                                                                                                     
+
+        printf("size of a charL %ld byte(s)\n", sizeof(c));
+
+	Printf(“size of an int:%ld byte(s)\n”, sizeof (l));
+	Printf(“size of a long int: %ld byte(s)\n”, sizeof(ll));
+	Printf(“size of a long long int: %ld byte(s)\n”, sizeof(lll));
+	Printf(“size of a float: %ld byte(s)\n”, sizeof(f));
+}
